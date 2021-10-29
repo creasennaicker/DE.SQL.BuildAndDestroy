4907926dddd5d5150f838332b4a41f33eeff55bb
creasennaicker <92214453+creasennaicker@users.noreply.github.com>
2021-10-29 02:02:27 -0400

    Update exercise10.sql

---
+++ answers/exercise10.sql
@@ -0,0 +1,7 @@
+mysql> ALTER TABLE users
+    -> ADD COLUMN (
+    -> middle_name VARCHAR(255) NOT NULL,
+    -> nick_name VARCHAR(255) NOT NULL,
+    -> email VARCHAR(255) NOT NULL,
+    -> suffix VARCHAR(64) NOT NULL,
+    -> badge_id VARCHAR(255) NOT NULL);
