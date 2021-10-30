aacbdf0b7219d5dec39a014bf99628c49fb6f93b
creasennaicker <92214453+creasennaicker@users.noreply.github.com>
2021-10-30 17:37:48 -0400

    Update exercise13.sql

--- answers/exercise13.sql
+++ answers/exercise13.sql
@@ -1,4 +1,4 @@
 mysql> ALTER TABLE users CHANGE FirstName First_Name VARCHAR(255) NOT NULL,
--> CHANGE LastName Last_name VARCHAR(255) NOT NULL;
+    -> CHANGE LastName Last_name VARCHAR(255) NOT NULL;
        
        
